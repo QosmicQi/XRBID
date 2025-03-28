f11703e6d7351007fd131fa48c14f0034611383d
Qiana Hunt <qianahunt@Qianas-MacBook-Pro.local>
2025-03-27 18:18:07 -0600

    Attempting to fix module issues

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	url='https://github.com/QosmicQi/XRBID',
 	author='Qiana Hunt',
 	author_email='qiana.hunt@uleth.ca',
-	packages=find_packages(where="XRBID"),
-    	package_dir={"": "XRBID"}
-	#py_modules=['Align', 'AutoPhots', 'CMDs', 'DataFrameMod', 'Headers', 'HRs', 'ImageSearch', 'Sources', 'WriteScript']
+	packages=find_packages(where="XRBID/XRBID"),
+    	package_dir={"": "XRBID/XRBID"}
+	py_modules=['Align', 'AutoPhots', 'CMDs', 'DataFrameMod', 'Headers', 'HRs', 'ImageSearch', 'Sources', 'WriteScript']
 )
