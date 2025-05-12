Major updates to WriteReg; Added CombineReg to WriteScript.py; Added new module XRTools.py; Fixed calculation error in CorrectAstrometry.

XRBID/Align.py
@@ -1,7 +1,11 @@
 ###################################################################################
-##########		For aligning various data, such as the		########### 
-##########		positions of HST images vs. positions 		###########
-##########		of a given region file, ect.			###########  
+##########		For aligning region files and calculating	###########
+##########		astrometric corrections and positional		###########
+##########		uncertainties. 					###########
+##########		Last Updated: May 12, 20205			###########
+##########		Added greater flexibility to CalcPU,		###########
+##########		added directionality to CorrectAstrometry,	###########
+##########		and removed unneeded functions.			###########
 ###################################################################################
 
 import re
@@ -23,141 +27,9 @@ from XRBID.WriteScript import WriteReg
 
 ###-----------------------------------------------------------------------------------------------------
 
-def AlignCoords(filenames=None, image=None, saveimg=None, region=None, savereg=None, coords=None, daofile=None, savecoords=None, showalign=False, showresids=True, addshift=None, getstd=False, pixtodeg=None, invertx=False, inverty=False): 
+def AlignReg(filename, shifts=None, outfile=None, pixtodeg=None): 
 
-	"""Algorithm for aligning the positions of points, given the initial base coordinates of objects and the offset coordinates for the same objects (usually HST vs. some region file). Allows for the analysis of the fit using residuals and standard deviation. Returns the necessary shifts. If trying to align HST filters using daofind coordinates, filenames should be given as the daofind region file for each filter, and the file with the list of IDs should be given as daofile. NOTE: Rotation still under construction."""
-
-	#pixtodeg=1.1e-05  # for M83
-	if pixtodeg == None: 
-		try: pixtodeg = fits.open(image[0])[1].header["CD2_2"]
-		except: pixtodeg = input("Please input pixel to degree conversion: ")
-
-	if filenames == None: # list of files containing the coordinates to compare
-		print("No coordinate files given.") 
-
-	xaligns = []
-	yaligns = []
-	stds = []
-
-	if coords != None:
-		if not isinstance(coords, list): coords=[coords]
-
-	if not isinstance(invertx, list): invertx= [invertx]
-	if not isinstance(inverty, list): inverty = [inverty]
-		
-		
-	if len(np.asarray(addshift).shape) == 1: addshift = [addshift]
-
-
-	# The first coordinate system is used as the base/standard
-	if daofile != None: 
-		dao = np.genfromtxt(daofile, dtype=int).T.tolist()
-		temp_x, temp_y = GetCoords(filenames[0], IDs=dao[0])
-		base_coords = np.array([[temp_x[j], temp_y[j]] for j in range(len(temp_x))])
-	else: base_coords = np.genfromtxt(filenames[0])
-
-	# For each file in filenames (excluding the first, which is the base)
-	for k in range(len(filenames[1:])):
-		i = filenames[1:][k] 
-		print("\nWorking on " + i)
-		try: 	
-			temp_x, temp_y = GetCoords(i, IDs=dao[k+1])
-			temp_align = np.array([[temp_x[j], temp_y[j]] for j in range(len(temp_x))])
-		except: temp_align = np.genfromtxt(i)	# temp_align contains coordinates in need of alignment
-
-		# If showalign = True, print the current alignment
-		if showalign: 
-			plt.figure()
-			plt.title("X Alignment")
-			plt.scatter(temp_align.T[0], base_coords.T[0])
-			plt.plot(np.linspace(0, max(temp_align.T[0])), np.linspace(0, max(temp_align.T[0])), linestyle="--", color="black", lw=0.5)
-			plt.show()
-			plt.figure()
-			plt.title("Y Alignment")
-			plt.scatter(temp_align.T[1], base_coords.T[1])
-			plt.plot(np.linspace(0, max(temp_align.T[1])), np.linspace(0, max(temp_align.T[1])), linestyle="--", color="black", lw=0.5)
-			plt.show()
-
-		# Calculate the offsets between each object and the base coordinates
-		temp_x = base_coords.T[0] - temp_align.T[0]
-		temp_y = base_coords.T[1] - temp_align.T[1]
-
-		# Sometimes the coordinates are inverted in the images
-		# Invert the offsets if this is the case
-		try: 
-			if invertx[k]: temp_x = -1*temp_x
-		except: 
-			if invertx[0]: temp_x = -1*temp_x
-		try: 
-			if inverty[k]: temp_y = -1*temp_y
-		except: 
-			if inverty[0]: temp_y = -1*temp_y
-
-		temp_stdx = std(temp_x)
-		temp_stdy = std(temp_y)
-
-		print("X temp: " + str(median(temp_x)) + " " + str(temp_stdx))
-		print("Y temp: " + str(median(temp_y)) + " " + str(temp_stdy))
-
-		# Calculate the median of the offsets, which is the accepted shift
-		try: 	# if addshift if given, include in the shift
-			xaligns.append(median(temp_x) + addshift[k][0])
-			yaligns.append(median(temp_y) + addshift[k][1])
-		except: # otherwise, simply use the median value
-			xaligns.append(median(temp_x))
-			yaligns.append(median(temp_y))
-		
-		print("Final shift: ", str(xaligns[-1]), str(yaligns[-1]), "\n")
-
-		stds.append(np.sqrt(temp_stdx**2 + temp_stdy**2))
-
-		
-		xresid = xaligns[-1] + (-1)**(invertx[0])*(temp_align.T[0] - base_coords.T[0])
-		yresid = yaligns[-1] + (-1)**(inverty[0])*(temp_align.T[1] - base_coords.T[1])
-
-		# By default, print the residual offsets after shifts
-		if showresids:
-			plt.figure()
-			plt.title("Residuals")
-			plt.scatter(xresid, yresid)
-			plt.show()
-		print("Median residuals: ", median(xresid), median(yresid))
-		print("Mean residuals: ", mean(xresid), mean(yresid))
-		print("Residuals STDs: ", std(xresid), std(yresid), "\n")
-
-		if image: AlignImg(filename=[image[k]], shifts=[xaligns[-1], yaligns[-1]], outfile=[saveimg[k]], pixtodeg=pixtodeg, image=image)
-
-			
-		# If a list of coords is given, update them. 
-		if coords: 
-			shift_coords = GetCoords(infile=coords[k])
-			shift_coords = np.array(shift_coords)
-			shift_coords[0] = shift_coords[0] + xaligns[k]
-			shift_coords[1] = shift_coords[1] + yaligns[k]
-			if not savecoords: savecoords = re.split(".", coords[k])[0] + "_shifted.coords"
-			
-			print("Saving " + savecoords)
-			with open(savecoords, 'w') as f: 
-	    			np.savetxt(f, np.column_stack([shift_coords[0], shift_coords[1]]), fmt="%s")
-
-	xalign_copy = []
-	yalign_copy = []
-
-	for i in range(len(xaligns)): 
-		xalign_copy.append(xaligns[i])
-		yalign_copy.append(yaligns[i])
-	
-	if savereg: AlignReg(filename=region, shifts=[xalign_copy, yalign_copy], outfile=savereg, pixtodeg=pixtodeg)
-
-	if getstd: return xaligns, yaligns, stds
-	else: return xaligns, yaligns
-		
-
-###-----------------------------------------------------------------------------------------------------
-
-def AlignReg(filename, shifts=None, outfile=None, pixtodeg=1.1e-05): 
-
-	"""Aligns all of the sources of a region file by a given translation or rotation. NOTE: Rotation still under construction."""
+	"""Aligns all of the sources of a region file by a given translation. """
 
 	if not filename: 
 		filename = input("Region file?: ")
@@ -241,51 +113,32 @@ def AlignReg(filename, shifts=None, outfile=None, pixtodeg=1.1e-05):
 
 ###-----------------------------------------------------------------------------------------------------
 
-def AlignImg(filename, shifts, outfile=None, image=None, pixtodeg=None): 
-
-	"""Realigns an image file by a given pixel translation or rotation. NOTE: Rotation still under construction. The pixtodeg alignment is taken from the values calculated for M83. Should be set using the CD1_1 header in each galaxy FITS file. """
-
-
-	print(shifts) 
-
-	# pixtodeg=1.1e-05  # for M83
-	if pixtodeg is None: 
-		try: pixtodeg = fits.open(image[0])[1].header["CD2_2"]
-		except: pixtodeg = input("Please input pixel to degree conversion: ")
-
-	if not isinstance(filename, list): filename = [filename]
-	if outfile and not isinstance(outfile, list): outfile = [outfile]
-	if len(np.asarray(shifts).shape) == 1: shifts = [shifts]
-
-	for k in range(len(filename)): 
-		img = fits.open(filename[k])
-		for j in range(len(img)): 
-			try: 
-				img[j].header["CRVAL1"] = img[j].header["CRVAL1"] + shifts[k][0]*pixtodeg
-				img[j].header["CRVAL2"] = img[j].header["CRVAL2"] + shifts[k][1]*pixtodeg
-			except: pass;
-		if not outfile: 
-			outimg = re.split(".", filename[k])[0]+"_aligned.fits"
-		else: outimg = outfile[k]
-
-		print("Saving " + outimg)
-		img.writeto(outimg, clobber=True)
-
-
-###-----------------------------------------------------------------------------------------------------
-
-def CorrectAstrometry(base_coords, cat_coords, autoresid=True, returnshifts=True, returncoords=False, savebasereg=False):
-  """A quick method for identifying the likely sources necessary for an aperture alignment.
+def CorrectAstrometry(base_coords, cat_coords, autoresid=False, returnshifts=True, returncoords=False, savebasereg=False):
+  """
+  A quick method for identifying the likely sources necessary for an aperture alignment.
   This will cross-reference between the two input coordinates to find common sources, calculate the median offset between these sources,
   and (limiting the residuals to identify the most likely true common sources) calculate the standard deviation of the median offsets.
 
-  PARAMETERS:
-  base_coords [array] : Coordinates of sources to be used as a base to which the catalog will be aligned.
-  cat_coords  [array] : Coordinates of the sources as seen in the misaligned catalog, for which the shifts will be calculated.
-  savebasereg [str]   : Saves a region file of the base coordinates of the matches sources from the base catalog
+  PARAMETERS
+  -----------
+  base_coords 	[array] : Coordinates of sources to be used as a base to which the catalog will be aligned. Typically, these are the 
+			  HST coordinates of XRBs.
+  cat_coords  	[array] : Coordinates of the sources as seen in the misaligned catalog, for which the shifts will be calculated.
+			  Typically, these are the Chandra coordinates of XRBs. 
+  autoresid	[bool]	: If True, automatically sets the limits of 'optimal' residuals from the calculated shift to +/- 0.02. 
+			  Otherwise, will prompt the user to select the best maximum and minimum residuals from a residual plot. 
+			  This allows the user to determine whether one or more selected base coordinate source should be excluded 
+			  from the astrometric correction for having residuals that are too far from the median. Default is False.
+  returnshifts	[bool]	: If True, returns the x- and y-axis shifts from the astrometric correction. Default is True. 
+  returncoords	[bool]	: If True, applies the astrometric corrections to the catalog coordinates and return the new coordinates as new_coords. 
+  savebasereg 	[str]   : Saves a region file of the base coordinates of the matches sources from the base catalog
+
+  RETURNS
+  -----------
+  new_coords 	[array]  		: New, shifted catalog coordinates after astrometric correction is applied (optional). 
+  medshiftx, medshifty, stdx, stdy 	: The x- and y-axis shifts and standard deviations on the astrometric corrections, 
+					  in units degrees. 
   
-  RETURNS:
-  new_coords [array]  : New, shifted catalog coordinates
   """
 
   # Setting the input coordinates as SkyCoord objects, which will allow us to calculate the alignment.
@@ -322,12 +175,14 @@ def CorrectAstrometry(base_coords, cat_coords, autoresid=True, returnshifts=True
   good_base_y = np.array(base_coords[1])[base_ids[good_matches]]
 
   # Calculating the full offsets between the input coordinates and the nearest items
-  offsets_x = good_cat_x - good_base_x
-  offsets_y = good_cat_y - good_base_y
+  #offsets_x = good_cat_x - good_base_x
+  #offsets_y = good_cat_y - good_base_y
+  offsets_x = good_base_x - good_cat_x
+  offsets_y = good_base_y - good_cat_y
 
   # Plotting the offsets
   fig, axes = plt.subplots(1,2, figsize=(6,4))
-  fig.suptitle("Offsets (deg)")
+  fig.suptitle("Offsets (deg)\n(Zoomed in and out)")
   axes[0].scatter(offsets_x, offsets_y)
   axes[0].axvline(x=np.median(offsets_x), color="black",linestyle="--", label="median")
   axes[0].axvline(x=np.mean(offsets_x), color="black", label="mean")
@@ -348,11 +203,13 @@ def CorrectAstrometry(base_coords, cat_coords, autoresid=True, returnshifts=True
   print("Median offsets in x and y (deg):", medshiftx, medshifty)
 
   # Plotting the residuals
-  resids_x = good_cat_x - medshiftx - good_base_x
-  resids_y = good_cat_y - medshifty - good_base_y
+  #resids_x = good_cat_x - medshiftx - good_base_x
+  #resids_y = good_cat_y - medshifty - good_base_y
+  resids_x = good_base_x - medshiftx - good_cat_x
+  resids_y = good_base_y - medshifty - good_cat_y
 
   fig, axes = plt.subplots(1,2, figsize=(6,4))
-  fig.suptitle("Residuals (deg)")
+  fig.suptitle("Residuals (deg)\n(Zoomed in and out)")
   axes[0].scatter(resids_x, resids_y)
   axes[0].axvline(x=np.median(resids_x), color="black",linestyle="--", label="median")
   axes[0].axvline(x=np.mean(resids_x), color="black", label="mean")
@@ -365,6 +222,7 @@ def CorrectAstrometry(base_coords, cat_coords, autoresid=True, returnshifts=True
   axes[1].axhline(y=np.mean(resids_y), color="black", label="mean")
   axes[1].set_xlim(median(resids_x)/1.5,median(resids_x)*1.5)
   axes[1].set_ylim(median(resids_y)/1.5,median(resids_y)*1.5)
+  plt.legend()
   plt.show()
 
   if not autoresid:
@@ -392,12 +250,12 @@ def CorrectAstrometry(base_coords, cat_coords, autoresid=True, returnshifts=True
   print("",stdx*3600, stdy*3600, "arcsecs\n")
 
   # Returns the new coordinates of the input catalog sources
-  new_coords = np.array([cat_coords[0] - medshiftx, cat_coords[1] - medshifty])
+  new_coords = np.array([cat_coords[0] + medshiftx, cat_coords[1] + medshifty])
     
   if savebasereg: 
     WriteReg(sources=[good_base_x.tolist(), good_base_y.tolist()],
-            color="red", outfile=savebasereg, coordsys="fk5", 
-             radius=0.15, radunit="arcsec")
+             color="red", outfile=savebasereg, coordsys="fk5", 
+             radius=1, radunit="arcsec")
   print("Check region file for correct assessments.")
 
   # Determining what to return
@@ -411,7 +269,7 @@ def CorrectAstrometry(base_coords, cat_coords, autoresid=True, returnshifts=True
 
 ###-----------------------------------------------------------------------------------------------------
 
-def CalcPU(df=False, theta=False, counts=False, std=[0,0], sig2search=False): 
+def CalcPU(df=False, theta=False, counts=False, theta_head="Theta", counts_head="Counts", std=[0,0], sig2search=False): 
 	
 	"""
 
@@ -422,22 +280,25 @@ def CalcPU(df=False, theta=False, counts=False, std=[0,0], sig2search=False):
 	------------
 	df		[pd.DataFrame]	: (optional) DataFrame containing the X-ray data for each source.
 					  This code assumes that the off-axis angle and net counts are 
-					  stored under the headers 'Theta' and 'Counts'. 
+					  stored under the headers 'Theta' and 'Counts', unless otherwise given. 
 	theta		[list, float]	: (optional) Off-axis angles of X-ray sources from the Chandra pointing,
 					  in units arcmin. This may be found as 'theta' or 'theta_mean' in the 
 					  Chandra Source Catalog. 
 	counts		[list, float]	: (optional) New counts of each X-ray source. 
+	theta_head 	[str]		: Header under which theta is stored in the DataFrame. Default is 'Theta'. 
+	counts_head	[str]		: Header under which counts in stored in the DataFrame. Default is 'Counts'.
 	std		[list] ([0,0])	: The standard deviation of the X-ray source son the HST image, in units
 					  arcseconds, along the x and y axis (e.g. [xstd,ystd]).
 	sig2search	[str] (False)	: In the event that an observation does not have a valid theta or counts
 					  value, allows the code to search for a default 2-sigma value under the 
 					  input header name. For example, CSC provides a 2-sigma major and minor 
-					  radii for the error ellipse of each source. By setting sig2search equal
-					  to the header under which the major radius is saved, user can request
-					  CalcPU to pull the major radius as the default 2-sigm positional 
+					  radii for the error ellipse of each source (err_ellipse_r0 and err_ellipse_r1). 
+					  By setting sig2search equal to the header under which the major radius is saved, 
+					  user can request CalcPU to pull the major radius as the default 2-sigma positional 
 					  uncertainty for all sources for which the Kim et al. 2007 calculation
 					  cannot be made. This prevents the code for returning artificially small 
 					  radii when the positional uncertainty cannot be calculated. 
+					  It's recommended to use the header under which err_ellipse_r0 is saved. 
 
 	RETURNS
 	---------
@@ -447,12 +308,14 @@ def CalcPU(df=False, theta=False, counts=False, std=[0,0], sig2search=False):
 	
 	# Off-axis angle should be in units arcminutes (default units for L19)
 	if not theta: 
-		theta = df["Theta"].values.tolist()
+		theta = df[theta_head].values.tolist()
 	if not counts: 
-		counts = df["Counts"].values.tolist()
+		counts = df[counts_head].values.tolist()
 
 	sig2 = []
 	sig1 = []
+
+	if not sig2search: print("WARNING: No default 2-sigma given. Some positional uncertainty values may be artificially small, unless sig2search is provided by user.")
 
 	# Calculating the Kim07 X-ray based uncertainties
 	for i in range(len(theta)):